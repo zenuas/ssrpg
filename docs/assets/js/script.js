b0dff3d3d8e7371446ae4202ff17e2c06de0a2dc
zenuas@gmail.com <zenuas@gmail.com>
2022-06-04 02:03:37 +0900

    敵武装の威力、装填時間、発射数、貫通力を記載

--- docs/assets/js/script.js
+++ docs/assets/js/script.js
@@ -14,29 +14,36 @@ $(window).on('load', function () {
 		$(list).attr("class", "commands");
 		const table = list.nextSibling.nextSibling;
 		$(table).tablesorter({headers: {12: { sorter: "text" }}});
-		table_col_visible(table, [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]);
+		table_col_visible(table, [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24]);
 		
 		const opts = [
 			{name: "武装",           visible: false, columns: [2, 3, 4]},
-			{name: "機関",           visible: false, columns: [5]},
-			{name: "ドロップ設計図", visible: false, columns: [6]},
-			{name: "カット率",       visible: false, columns: [7, 8, 9]},
-			{name: "回避率",         visible: false, columns: [10, 11]},
-			{name: "回復間隔",       visible: false, columns: [12]},
-			{name: "登場ステージ",   visible: true,  columns: [13]}
+			{name: "武装威力",       visible: false, columns: [5, 9, 13]},
+			{name: "武装装填時間",   visible: false, columns: [6, 10, 14]},
+			{name: "武装発射数",     visible: false, columns: [7, 11, 15]},
+			{name: "武装貫通力",     visible: false, columns: [8, 12, 16]},
+			{name: "機関",           visible: false, columns: [17]},
+			{name: "ドロップ設計図", visible: false, columns: [18]},
+			{name: "カット率",       visible: false, columns: [19, 20, 21]},
+			{name: "回避率",         visible: false, columns: [22, 23]},
+			{name: "回復間隔",       visible: false, columns: [24]},
+			{name: "登場ステージ",   visible: true,  columns: [25]}
 		];
 		
 		opts
 			.map((opt) => {
-				const v = $("<span>" + (opt.visible ? "非表示" : "表示") + "</span>")[0];
-				const a = $("<a href='javascript:void(0)' class='box'>" + opt.name + "</a>")[0];
+				const v  = $("<span>" + (opt.visible ? "非表示" : "表示") + "</span>")[0];
+				const a  = $("<a href='javascript:void(0)' class='box'>" + opt.name + "</a>")[0];
+				const li = $("<li class='buttons" + (opt.visible ? " pushed" : "") + "'></li>").append(a);
 				a.append(v);
 				a.onclick = () => {
 					table_col_visible(table, opt.columns);
 					opt.visible = !opt.visible;
 					$(v).text(opt.visible ? "非表示" : "表示");
+					li.removeClass("pushed");
+					if(opt.visible) li.addClass("pushed");
 				};
-				return $("<li class='buttons'></li>").append(a);
+				return li;
 			})
 			.forEach((x) => x.appendTo(list));
 	});
